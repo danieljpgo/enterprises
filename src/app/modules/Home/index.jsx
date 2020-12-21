a093d63e3f7c7a956a19d667e6b68bb7fd418735
Daniel Jorge <djpgonzaga@gmail.com>
2020-12-21 03:04:03 -0300

    feat: add animation to home page

--- src/app/modules/Home/index.jsx
+++ src/app/modules/Home/index.jsx
@@ -1,8 +1,14 @@
 import { Container } from './styles';
 import Text from '../../common/components/Text';
+import { fadeInOut } from '../../common/utils/helpers/animations';
 
 const Home = () => (
-  <Container>
+  <Container
+    variants={fadeInOut}
+    initial="hidden"
+    animate="show"
+    exit="out"
+  >
     <Text variants="sub">Clique na busca para iniciar.</Text>
   </Container>
 );
